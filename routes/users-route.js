51e6d1c9f71fc67c60949d9d0cdf9df10ddaa6db
0xmuhammed9 <muhammed9alii9@gmail.com>
2025-02-01 15:06:46 +0200

    Update users-route.js

--- routes/users-route.js
+++ routes/users-route.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import signupUser from '../controllers/auth-controller.js';
-import { createUser } from '../controllers/users-controller.js';
+
 
 const usersRoute = express.Router();
 
 usersRoute.route('/signup').post(signupUser);
 
-usersRoute.route('/').get().post(createUser);
+usersRoute.route('/').get().post();
 usersRoute.route('/:id').get().delete().patch();
 
 export default usersRoute;
