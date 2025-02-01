8994d5c9d9fa42fbb1a6593d779780f6b7b48241
0xmuhammed9 <muhammed9alii9@gmail.com>
2025-02-01 15:02:10 +0200

    Update users-controller.js

---
+++ controllers/users-controller.js
@@ -0,0 +1,16 @@
+import User from '../models/users-model.js';
+
+const createUser = async (req, res, next) => {
+  const newUser = await User.create(req.body);
+
+  res.status(201).json({
+    status: 'success',
+    data: {
+      user: newUser,
+    },
+  });
+};
+
+export default createUser;
+
+
