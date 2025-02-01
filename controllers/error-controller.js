fce440c2f1c1ade06190c9e9ba1977ef77ff735b
0xmuhammed9 <muhammed9alii9@gmail.com>
2025-02-01 19:20:08 +0200

    Adding castErrorDB Handling

--- controllers/error-controller.js
+++ controllers/error-controller.js
@@ -1,3 +1,41 @@
+import AppError from '../utils/appError.js';
+/**
+ * @param {import ('../utils/appError').default} err
+ * @param {import ('express').Response} res
+ */
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    error: err,
+    stack: err.stack,
+  });
+};
+
+/**
+ * @param {import ('express').Response} res
+ * @param {import ('../utils/appError').default} err
+ */
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.error('Error:', err);
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went wrong!',
+    });
+  }
+};
+
+const castErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+}
+
 /**
  * @param {import ('express').Request} req
  * @param {import ('express').Response} res
@@ -7,10 +45,18 @@
 const errorHandler = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  }
+  if (process.env.NODE_ENV === 'production') {
+    let error = Object.create(Object.getPrototypeOf(err));
+    Object.assign(error, err);
+    
+    if(error.name === 'CastError') error = castErrorDB(error);
+    
+    sendErrorProd(error, res);
+  }
 };
 
 export default errorHandler;
