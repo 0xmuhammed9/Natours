8ea96a1bc99d1d7f32b6039e2a97ca02defc8fc6
0xmuhammed9 <muhammed9alii9@gmail.com>
2025-02-02 14:23:56 +0200

    Update server.js
    Adding UnhandledRejection and UncaughtException

--- server.js
+++ server.js
@@ -8,9 +8,24 @@ mongoose.connect(NATOURS_DB).then(() => {
   console.log('Connected to the Database Successfully :)');
 });
 
+process.on('uncaughtException', (err) => {
+  console.log('Uncaught Exception! Shutting Down...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
+
 const server = app.listen(PORT, () => {
   if (process.env.NODE_ENV === 'development')
     console.log(`We're Listening on Port:${PORT}, Development Mode`);
   else if (process.env.NODE_ENV === 'production')
     console.log(`We're Listening on Port:${PORT}, Production Mode`);
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log('Unhandled Rejection! Shutting Down...');
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
