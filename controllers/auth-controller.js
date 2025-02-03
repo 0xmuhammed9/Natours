d415e03ac33aea99bc161a7364825b96df289fd7
0xmuhammed9 <muhammed9alii9@gmail.com>
2025-02-03 12:40:27 +0200

    Update auth-controller.js
    This edit only allow the data that we actually need to be put into the new User

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -2,14 +2,18 @@ import usersModel from '../models/users-model.js';
 import catchAsync from '../utils/catchAsync.js';
 
 const signupUser = catchAsync(async (req, res) => {
-    const newUser = await usersModel.create(req.body);
-    res.status(201).json({
-      status: 'success',
-      data: {
-        user: newUser,
-      },
-    });
+  const newUser = await usersModel.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+  res.status(201).json({
+    status: 'success',
+    data: {
+      user: newUser,
+    },
+  });
 });
-
 
 export default signupUser;
