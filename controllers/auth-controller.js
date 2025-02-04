ff102cfce708376fb014e16b1361b8b6a3f4e989
0xmuhammed9 <muhammed9alii9@gmail.com>
2025-02-04 14:44:20 +0200

    Update auth-controller.js
    Add protect route function, check if token is exist or not

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -47,4 +47,28 @@ const loginUser = catchAsync(async (req, res, next) => {
   });
 });
 
-export { signupUser, loginUser };
+const protect = catchAsync((req, res, next) => {
+  // 1) Getting token and check if it's there
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
+  if (!token) {
+    return next(
+      new AppError('You are not logged in! Please log in to get access.', 401)
+    );
+  }
+
+  //2) Verification of token
+
+  //3) Check if user still exists
+
+  //4) Check if user changed password after the token was issued
+
+  next();
+});
+
+export { signupUser, loginUser,protect};
