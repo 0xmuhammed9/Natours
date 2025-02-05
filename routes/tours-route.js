4bf55cc8618ff50db8af05823baa3af3645732a1
0xmuhammed9 <muhammed9alii9@gmail.com>
2025-02-05 20:49:30 +0200

    Update tours-route.js

--- routes/tours-route.js
+++ routes/tours-route.js
@@ -6,10 +6,15 @@ import {
   deleteTour,
   addTour,
 } from '../controllers/tours-controller.js';
+import { protect, isRestricted } from '../controllers/auth-controller.js';
 
 const toursRoute = express.Router();
 
-toursRoute.route('/').get(getAllTour).post(addTour);
-toursRoute.route('/:id').get(getTour).delete(deleteTour).patch(updateTour);
+toursRoute.route('/').get(protect, getAllTour).post(addTour);
+toursRoute
+  .route('/:id')
+  .get(getTour)
+  .delete(protect, isRestricted(['admin ','lead-guide']), deleteTour)
+  .patch(updateTour);
 
 export default toursRoute;
