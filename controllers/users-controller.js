15a9b9f57b88eff98f96c06f6378ef61212a41b1
0xmuhammed9 <muhammed9alii9@gmail.com>
2025-02-14 19:07:11 +0200

    Update users-controller.js

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,2 +1,58 @@
+import userModel from '../models/users-model.js';
+import AppError from '../utils/appError.js';
+import catchAsync from '../utils/catchAsync.js';
 
+/**
+ * ********************************************************************************************************************
+ *                                                  Helper Functions
+ * ********************************************************************************************************************
+ */
+const filterObj = (obj, ...allowedObj) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedObj.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj; // Move return statement outside forEach
+};
 
+/**
+ * ********************************************************************************************************************
+ *                                                  Controller Functions
+ * ********************************************************************************************************************
+ */
+export const updateMe = catchAsync(async (req, res, next) => {
+  //1) Create error if user POSTs password Data.
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password updates. Please use /updateMyPassword',
+        400
+      )
+    );
+  }
+  //2) Filter unwanted fields that are not allowed to be updated
+  const filteredBody = filterObj(req.body, 'name', 'email');
+  //3) Update user document
+  const updatedUser = await userModel.findByIdAndUpdate(
+    req.user.id,
+    filteredBody,
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
+    },
+  });
+});
+
+export const deleteMe = catchAsync(async (req, res, next) => {
+  await userModel.findByIdAndUpdate(req.user.id, { active: false });
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
