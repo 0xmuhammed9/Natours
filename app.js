60a23c7e070b2aaf4f72c95e279c93c4d0b26e2c
0xmuhammed9 <muhammed9alii9@gmail.com>
2025-02-22 13:59:23 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,12 +5,38 @@ import toursRoute from './routes/tours-route.js';
 import usersRoute from './routes/users-route.js';
 import AppError from './utils/appError.js';
 import errorHandler from './controllers/error-controller.js';
-
+import { rateLimit } from 'express-rate-limit';
+import helmet from 'helmet';
+import xss from 'xss-clean';
+import DataSansitize from 'express-mongo-sanitize';
+import hpp from 'hpp';
 const app = express();
 dotenv.config();
 
 // Middleware
+app.use(helmet());
+app.use(
+  '/api',
+  rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+  })
+);
 app.use(express.json());
+app.use(DataSansitize());
+app.use(xss());
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'price',
+      'maxGroupSize',
+      'difficulty',
+      'ratingsAverage',
+      'ratingsQuantity',
+    ],
+  })
+);
 app.use(morgan('dev'));
 
 // Routes
