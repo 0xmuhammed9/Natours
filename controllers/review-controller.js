bb48e6ad0fd11e5e296e3f508d0b32f4ab28cc6d
0xmuhammed9 <muhammed9alii9@gmail.com>
2025-02-26 14:53:00 +0200

    Update review-controller.js

--- controllers/review-controller.js
+++ controllers/review-controller.js
@@ -1,11 +1,13 @@
 import Review from '../models/reviews-model.js';
+import AppError from '../utils/appError.js';
 import catchAsync from '../utils/catchAsync.js';
-import { deleteOne } from './handlerFactory.js';
+import { deleteOne, createOne } from './handlerFactory.js';
 
 const getAllReviews = catchAsync(async (req, res, next) => {
   let filter = {};
   if (req.params.tourId) filter = { tour: req.params.tourId };
   const Reviews = await Review.find(filter);
+  if (Reviews.length === 0) return next(new AppError('No reviews found', 404));
   res.status(200).json({
     status: 'success',
     reslut: Reviews.length,
@@ -15,18 +17,7 @@ const getAllReviews = catchAsync(async (req, res, next) => {
   });
 });
 
-const createReview = catchAsync(async (req, res, next) => {
-  if (!req.body.tour) req.body.tour = req.params.tourId;
-  if (!req.body.user) req.body.user = req.user.id;
-  const newReview = await Review.create(req.body);
-  res.status(201).json({
-    status: 'success',
-    data: {
-      newReview,
-    },
-  });
-});
-
+const createReview = createOne(Review);
 const deleteReview = deleteOne(Review);
 
 export { createReview, getAllReviews, deleteReview };
