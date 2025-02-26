118250597af8befa3d9dd61f31a37f370c424c4e
0xmuhammed9 <muhammed9alii9@gmail.com>
2025-02-26 14:52:56 +0200

    Update handlerFactory.js
    Add createOne, UpdateOne

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -13,4 +13,30 @@ const deleteOne = (Model) =>
     });
   });
 
-export { deleteOne };
+const updateOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!doc) return next(new AppError('No document found with that ID', 404));
+    res.status(200).json({
+      status: 'Success',
+      data: {
+        data: doc,
+      },
+    });
+  });
+
+const createOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.create(req.body);
+    res.status(201).json({
+      status: 'Success',
+      data: {
+        data: doc,
+      },
+    });
+  });
+
+export { deleteOne, updateOne, createOne };
