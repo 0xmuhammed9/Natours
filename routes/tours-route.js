47dee456392f0abf5e4a792fe76b1927b8f40cf7
0xmuhammed9 <muhammed9alii9@gmail.com>
2025-02-26 13:13:06 +0200

    Update tours-route.js
    Add Nested Route

--- routes/tours-route.js
+++ routes/tours-route.js
@@ -7,14 +7,17 @@ import {
   addTour,
 } from '../controllers/tours-controller.js';
 import { protect, isRestricted } from '../controllers/auth-controller.js';
+import reviewRoute from './reviews-route.js';
 
 const toursRoute = express.Router();
+
+toursRoute.use('/:tourId/reviews', reviewRoute);
 
 toursRoute.route('/').get(protect, getAllTour).post(addTour);
 toursRoute
   .route('/:id')
   .get(getTour)
-  .delete(protect, isRestricted(['admin ','lead-guide']), deleteTour)
+  .delete(protect, isRestricted(['admin ', 'lead-guide']), deleteTour)
   .patch(updateTour);
 
 export default toursRoute;
