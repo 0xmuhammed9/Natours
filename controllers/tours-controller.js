729085b0db70e967f6e8b4f679103c38fba50eec
0xmuhammed9 <muhammed9alii9@gmail.com>
2025-02-26 14:53:06 +0200

    Update tours-controller.js

--- controllers/tours-controller.js
+++ controllers/tours-controller.js
@@ -2,13 +2,8 @@ import Tour from '../models/tours-model.js';
 import APIQuery from '../utils/apiFeatures.js';
 import AppError from '../utils/appError.js';
 import catchAsync from '../utils/catchAsync.js';
-import { deleteOne } from './handlerFactory.js';
+import { createOne, deleteOne, updateOne } from './handlerFactory.js';
 
-/**
- * @param {import ('express').Request} req
- * @param {import ('express').Response} res
- * @param {import ('express').NextFunction} next
- */
 const getAllTour = catchAsync(async (req, res, next) => {
   const Query = new APIQuery(Tour.find(), req.query)
     .filter()
@@ -42,41 +37,8 @@ const getTour = catchAsync(async (req, res, next) => {
   });
 });
 
-const addTour = catchAsync(async (req, res, next) => {
-  const createdTour = await Tour.create(req.body);
-  res.status(200).json({
-    status: 'Success',
-    data: {
-      createdTour,
-    },
-  });
-});
-
-/**
- *
- * @param {import ('express').Request} req
- * @param {import ('express').Response} res
- * @param {import ('express').NextFunction} next
- */
-const updateTour = catchAsync(async (req, res, next) => {
-  const updatedTour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true,
-  });
-
-  if (!updatedTour) {
-    return next(new AppError('No tour found with that ID', 404));
-  }
-  res.status(200).json({
-    status: 'Success',
-    data: {
-      updatedTour,
-    },
-  });
-});
-
-
+const addTour = createOne(Tour);
+const updateTour = updateOne(Tour);
 const deleteTour = deleteOne(Tour);
- 
 
 export { getAllTour, getTour, updateTour, deleteTour, addTour };
