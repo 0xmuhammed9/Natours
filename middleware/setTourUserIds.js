96d2aacd8cfbac88811bd292aff237bc3befceb7
0xmuhammed9 <muhammed9alii9@gmail.com>
2025-02-27 17:55:06 +0200

    Delete setTourUserIds.js

--- middleware/setTourUserIds.js
+++
@@ -1,8 +0,0 @@
-const setTourUserIds = (req, res, next) => {
-  if (!req.body.tour) req.body.tour = req.params.tourId;
-  if (!req.body.user) req.body.user = req.user.id;
-  next();
-};
-
-
-export default setTourUserIds;
