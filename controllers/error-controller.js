8377959801cbd6e6e23a62aeca2da6ae099ee5df
0xmuhammed9 <muhammed9alii9@gmail.com>
2025-02-27 17:54:54 +0200

    Update error-controller.js

--- controllers/error-controller.js
+++ controllers/error-controller.js
@@ -1,64 +1,13 @@
-import AppError from '../utils/appError.js';
-/**
- * @param {import ('../utils/appError').default} err
- * @param {import ('express').Response} res
- */
-const sendErrorDev = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-    error: err,
-    stack: err.stack,
-  });
-};
+import {
+  castErrorDB,
+  handleDuplicateFieldsDB,
+  handleValidationErrorDB,
+  handleJWTError,
+  handleJWTExpiredError,
+  sendErrorDev,
+  sendErrorProd,
+} from '../utils/errorFunctions.js';
 
-/**
- * @param {import ('express').Response} res
- * @param {import ('../utils/appError').default} err
- */
-const sendErrorProd = (err, res) => {
-  if (err.isOperational) {
-    res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message,
-    });
-  } else {
-    console.error('Error:', err);
-    res.status(500).json({
-      status: 'error',
-      message: 'Something went wrong!',
-    });
-  }
-};
-
-const castErrorDB = (err) => {
-  const message = `Invalid ${err.path}: ${err.value}`;
-  return new AppError(message, 400);
-};
-
-const handleDuplicateFieldsDB = (err) => {
-  const message = `Duplicate field value: ${err.keyValue.name}. Please use another value!`;
-  return new AppError(message, 400);
-};
-
-const handleValidationErrorDB = (err) => {
-  const message = Object.values(err.errors)
-    .map((el) => el.message)
-    .join('. ');
-  return new AppError(message, 400);
-};
-
-const handleJWTError = () =>
-  new AppError('Invalid token. Please log in again!', 401);
-const handleJWTExpiredError = () =>
-  new AppError('Your token has expired! Please log in again.', 401);
-
-/**
- * @param {import ('express').Request} req
- * @param {import ('express').Response} res
- * @param {import ('express').NextFunction} next
- * @param {import ('../utils/appError').default} err
- */
 const errorHandler = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
