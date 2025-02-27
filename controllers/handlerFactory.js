53ec5c970a5cea2a4d0b9b34bd90a8b9732ee3be
0xmuhammed9 <muhammed9alii9@gmail.com>
2025-02-27 17:43:40 +0200

    Update handlerFactory.js
    Add getOne, getAll

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -1,5 +1,41 @@
 import catchAsync from '../utils/catchAsync.js';
 import AppError from '../utils/appError.js';
+import APIQuery from '../utils/apiFeatures.js';
+
+const getAll = (Model) =>
+  catchAsync(async (req, res, next) => {
+    let filter = {};
+    if (req.params.tourId) filter = { tour: req.params.tourId };
+    const Query = new APIQuery(Model.find(), req.query)
+      .filter()
+      .sort()
+      .fields()
+      .pagination();
+    const doc = await Query.query;
+    res.status(200).json({
+      status: 'Success',
+      results: doc.length,
+      data: {
+        doc,
+      },
+    });
+  });
+
+const getOne = (Model, popOptions) =>
+  catchAsync(async (req, res, next) => {
+    let query = Model.findById(req.params.id);
+    if (popOptions) query = query.populate(popOptions);
+    const doc = await query;
+    if (!doc) {
+      return next(new AppError('No document found with that ID', 404));
+    }
+    res.status(200).json({
+      status: 'Success',
+      data: {
+        data: doc,
+      },
+    });
+  });
 
 const deleteOne = (Model) =>
   catchAsync(async (req, res, next) => {
@@ -39,4 +75,4 @@ const createOne = (Model) =>
     });
   });
 
-export { deleteOne, updateOne, createOne };
+export { getAll, deleteOne, updateOne, createOne, getOne };
