2181d4253b44cdc6996297ae71a216379c15c291
0xmuhammed9 <muhammed9alii9@gmail.com>
2025-02-27 17:54:57 +0200

    Update review-controller.js

--- controllers/review-controller.js
+++ controllers/review-controller.js
@@ -1,9 +1,23 @@
 import Review from '../models/reviews-model.js';
-import { deleteOne, createOne, getAll, getOne } from './handlerFactory.js';
+import { deleteOne, createOne, getAll, getOne, updateOne } from './handlerFactory.js';
+
+
+/**
+ * ******************************************************************************************************
+ *                                                     Module Functions
+ * ******************************************************************************************************
+ */
 
 const getAllReviews = getAll(Review);
 const getOneReview = getOne(Review);
 const createReview = createOne(Review);
+const updateReview = updateOne(Review);
 const deleteReview = deleteOne(Review);
 
-export { createReview, getAllReviews, deleteReview, getOneReview };
+export {
+  createReview,
+  getAllReviews,
+  deleteReview,
+  getOneReview,
+  updateReview,
+};
