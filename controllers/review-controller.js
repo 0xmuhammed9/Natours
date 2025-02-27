7f3d474b7612772ed71cb1ad6ceede51fd614beb
0xmuhammed9 <muhammed9alii9@gmail.com>
2025-02-27 17:43:44 +0200

    Update review-controller.js

--- controllers/review-controller.js
+++ controllers/review-controller.js
@@ -1,23 +1,9 @@
 import Review from '../models/reviews-model.js';
-import AppError from '../utils/appError.js';
-import catchAsync from '../utils/catchAsync.js';
-import { deleteOne, createOne } from './handlerFactory.js';
+import { deleteOne, createOne, getAll, getOne } from './handlerFactory.js';
 
-const getAllReviews = catchAsync(async (req, res, next) => {
-  let filter = {};
-  if (req.params.tourId) filter = { tour: req.params.tourId };
-  const Reviews = await Review.find(filter);
-  if (Reviews.length === 0) return next(new AppError('No reviews found', 404));
-  res.status(200).json({
-    status: 'success',
-    reslut: Reviews.length,
-    data: {
-      Reviews,
-    },
-  });
-});
-
+const getAllReviews = getAll(Review);
+const getOneReview = getOne(Review);
 const createReview = createOne(Review);
 const deleteReview = deleteOne(Review);
 
-export { createReview, getAllReviews, deleteReview };
+export { createReview, getAllReviews, deleteReview, getOneReview };
