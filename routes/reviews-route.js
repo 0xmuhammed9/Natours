ab16d8ffbb881a04d39c4291a41a18c54ee51984
0xmuhammed9 <muhammed9alii9@gmail.com>
2025-02-27 17:55:08 +0200

    Update reviews-route.js

--- routes/reviews-route.js
+++ routes/reviews-route.js
@@ -5,8 +5,9 @@ import {
   createReview,
   deleteReview,
   getOneReview,
+  updateReview,
 } from '../controllers/review-controller.js';
-import setTourUserIds from '../middleware/setTourUserIds.js';
+import setTourUserIds from '../utils/setTourUserIds.js';
 
 const reviewRoute = express.Router({
   mergeParams: true,
@@ -14,6 +15,10 @@ const reviewRoute = express.Router({
 reviewRoute.use(protect);
 
 reviewRoute.route('/').get(getAllReviews).post(setTourUserIds, createReview);
-reviewRoute.route('/:id').delete(deleteReview).get(getOneReview);
+reviewRoute
+  .route('/:id')
+  .delete(deleteReview)
+  .get(getOneReview)
+  .patch(updateReview);
 
 export default reviewRoute;
