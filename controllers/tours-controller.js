32bdb119144fe4ecf4b9c02c74d491ea2a667836
0xmuhammed9 <muhammed9alii9@gmail.com>
2025-03-02 13:47:37 +0200

    Update tours-controller.js

--- controllers/tours-controller.js
+++ controllers/tours-controller.js
@@ -6,7 +6,8 @@ import {
   getOne,
   updateOne,
 } from './handlerFactory.js';
-
+import catchAsync from '../utils/catchAsync.js';
+import sendResponse from '../utils/sendResponse.js';
 
 /**
  * ******************************************************************************************************
@@ -20,4 +21,70 @@ const addTour = createOne(Tour);
 const updateTour = updateOne(Tour);
 const deleteTour = deleteOne(Tour);
 
-export { getAllTour, getTour, updateTour, deleteTour, addTour };
+const getToursWithin = catchAsync(async (req, res, next) => {
+  const { distance, latlng, unit } = req.params;
+  const [lat, lng] = latlng.split(',');
+
+  const radius = unit === 'mi' ? distance / 3963.2 : distance / 6378.1;
+
+  if (!lat || !lng) {
+    next(
+      new AppError(
+        'Please provide latitutr and longitude in the format lat,lng.',
+        400
+      )
+    );
+  }
+
+  const tours = await Tour.find({
+    startLocation: { $geoWithin: { $centerSphere: [[lng, lat], radius] } },
+  });
+
+  sendResponse(res, 200, tours);
+});
+const getDistances = catchAsync(async (req, res, next) => {
+  const { latlng, unit } = req.params;
+  const [lat, lng] = latlng.split(',');
+
+  const multiplier = unit === 'mi' ? 0.000621371 : 0.001;
+
+  if (!lat || !lng) {
+    next(
+      new AppError(
+        'Please provide latitutr and longitude in the format lat,lng.',
+        400
+      )
+    );
+  }
+
+  const distances = await Tour.aggregate([
+    {
+      $geoNear: {
+        near: {
+          type: 'Point',
+          coordinates: [lng * 1, lat * 1],
+        },
+        distanceField: 'distance',
+        distanceMultiplier: multiplier,
+      },
+    },
+    {
+      $project: {
+        distance: 1,
+        name: 1,
+      },
+    },
+  ]);
+
+  sendResponse(res, 200, distances);
+});
+
+export {
+  getAllTour,
+  getTour,
+  updateTour,
+  deleteTour,
+  addTour,
+  getToursWithin,
+  getDistances,
+};
