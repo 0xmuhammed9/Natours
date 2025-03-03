ac3448d32226e5d42b3eb7e8ee1961438ab33420
0xmuhammed9 <muhammed9alii9@gmail.com>
2025-03-03 17:40:35 +0200

    Update view-route.js

--- routes/view-route.js
+++ routes/view-route.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getView, getTour } from '../controllers/view-controller.js';
+import { getView, getTour, login } from '../controllers/view-controller.js';
 
 const viewRouter = express.Router();
 
 viewRouter.get('/', getView);
-viewRouter.get('/tour', getTour);
+viewRouter.get('/tour/:slug', getTour);
+viewRouter.get('/login', login);
 
 export default viewRouter;
