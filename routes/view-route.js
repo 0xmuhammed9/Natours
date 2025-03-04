cf79818cb52b4484ba9c4d358ccf4c260e9deb29
0xmuhammed9 <muhammed9alii9@gmail.com>
2025-03-04 17:50:03 +0200

    Update view-route.js

--- routes/view-route.js
+++ routes/view-route.js
@@ -5,8 +5,10 @@ import {
   login,
   signup,
 } from '../controllers/view-controller.js';
+import { isLoggedIn } from '../controllers/auth-controller.js';
 
 const viewRouter = express.Router();
+viewRouter.use(isLoggedIn);
 
 viewRouter.get('/', getView);
 viewRouter.get('/tour/:slug', getTour);
