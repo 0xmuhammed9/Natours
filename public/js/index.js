f7b4097103af6c6c9873d39ff316ef0e974824af
0xmuhammed9 <muhammed9alii9@gmail.com>
2025-03-05 21:53:40 +0200

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,18 @@
-import { login } from './login.js';
+/* eslint-disable */
+import { login, logout } from './login.js';
+import { showAlert } from './alerts.js';
 
+// DOM ELEMENTS
 const loginForm = document.querySelector('.form--login');
+const logoutBtn = document.querySelector('.nav__el--logout');
 
+// DELEGATION
 if (loginForm)
-    loginForm.addEventListener('submit', e => {
-      e.preventDefault();
-      const email = document.getElementById('email').value;
-      const password = document.getElementById('password').value;
-      login(email, password);
-    });
+  loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    login(email, password);
+  });
+
+if (logoutBtn) logoutBtn.addEventListener('click', logout);
