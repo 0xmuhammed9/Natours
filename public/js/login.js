1a440fce2c651e65c89ddfbea8126b1eb8946534
0xmuhammed9 <muhammed9alii9@gmail.com>
2025-03-05 21:53:44 +0200

    Update login.js

--- public/js/login.js
+++ public/js/login.js
@@ -31,3 +31,22 @@ export const login = async (email, password) => {
     );
   }
 };
+
+export const logout = async () => {
+  try {
+    const res = await axios({
+      method: 'GET',
+      url: '/api/v1/users/logout',
+      withCredentials: true,
+    });
+
+    if (res.data.status === 'success') {
+      showAlert('success', 'Logged out successfully!');
+      window.setTimeout(() => {
+        location.assign('/');
+      }, 1500);
+    }
+  } catch (err) {
+    showAlert('error', 'Error logging out! Try again.');
+  }
+};
