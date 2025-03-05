bf9c646444452ec88eaf974821f394109b4b5de6
0xmuhammed9 <muhammed9alii9@gmail.com>
2025-03-05 17:28:01 +0200

    Update view-controller.js

--- controllers/view-controller.js
+++ controllers/view-controller.js
@@ -6,6 +6,7 @@ const getView = catchAsync(async (req, res, next) => {
   res.status(200).render('overview', {
     title: 'All Tours Overview',
     tours,
+    user: req.user,
   });
 });
 
@@ -18,18 +19,21 @@ const getTour = catchAsync(async (req, res, next) => {
   res.status(200).render('tour', {
     title: 'The Forest Hiker Tour',
     tour,
+    user: req.user,
   });
 });
 
 const login = catchAsync(async (req, res, next) => {
   res.status(200).render('login', {
     title: 'Log into your account',
+    user: req.user,
   });
 });
 const signup = catchAsync(async (req, res, next) => {
   res.status(200).render('signup', {
     title: 'Create your account',
+    user: req.user,
   });
 });
 
-export { getTour, getView, login,signup };
+export { getTour, getView, login, signup };
