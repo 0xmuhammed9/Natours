5d8208ca5a3c3d343d659fedddf9a3ad9a884df0
0xmuhammed9 <muhammed9alii9@gmail.com>
2025-03-06 16:28:04 +0200

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,13 @@
 /* eslint-disable */
 import { login, logout } from './login.js';
 import { showAlert } from './alerts.js';
+import { updateSettings } from './updateSetting.js';
 
-// DOM ELEMENTS
 const loginForm = document.querySelector('.form--login');
 const logoutBtn = document.querySelector('.nav__el--logout');
+const userDataForm = document.querySelector('.form-user-data');
+const userPasswordForm = document.querySelector('.form-user-password');
 
-// DELEGATION
 if (loginForm)
   loginForm.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -16,3 +17,38 @@ if (loginForm)
   });
 
 if (logoutBtn) logoutBtn.addEventListener('click', logout);
+if (userDataForm)
+  userDataForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    updateSettings({ name, email }, 'data');
+  });
+
+  if (userDataForm)
+    userDataForm.addEventListener('submit', e => {
+      e.preventDefault();
+      const name = document.getElementById('name').value;
+      const email = document.getElementById('email').value;
+      updateSettings({ name, email }, 'data');
+    });
+  
+  if (userPasswordForm)
+    userPasswordForm.addEventListener('submit', async e => {
+      e.preventDefault();
+      document.querySelector('.btn--save-password').textContent = 'Updating...';
+  
+      const password = document.getElementById('password-current').value;
+      const newPassword = document.getElementById('password').value;
+      const passwordConfirm = document.getElementById('password-confirm').value;
+      await updateSettings(
+        { password, newPassword, passwordConfirm },
+        'password'
+      );
+  
+      document.querySelector('.btn--save-password').textContent = 'Save password';
+      document.getElementById('password-current').value = '';
+      document.getElementById('password').value = '';
+      document.getElementById('password-confirm').value = '';
+    });
+  
