144f4cccc2e9000c637332e559dab09a15e84343
0xmuhammed9 <muhammed9alii9@gmail.com>
2025-03-07 17:40:38 +0200

    Update view-controller.js

--- controllers/view-controller.js
+++ controllers/view-controller.js
@@ -1,11 +1,14 @@
 import Tour from '../models/tours-model.js';
+import User from '../models/users-model.js';
 import catchAsync from '../utils/catchAsync.js';
+import AppError from '../utils/appError.js';
 
 const getView = catchAsync(async (req, res, next) => {
   const tours = await Tour.find();
   res.status(200).render('overview', {
     title: 'All Tours Overview',
     tours,
+    user: req.user,
   });
 });
 
@@ -15,28 +18,52 @@ const getTour = catchAsync(async (req, res, next) => {
     path: 'reviews',
     fields: 'review rating user',
   });
+  if (!tour) {
+    return next(new AppError('There is no tour with that name', 404));
+  }
   res.status(200).render('tour', {
     title: 'The Forest Hiker Tour',
     tour,
+    user: req.user,
   });
 });
-
 
 const login = catchAsync(async (req, res, next) => {
   res.status(200).render('login', {
     title: 'Log into your account',
+    user: req.user,
   });
 });
 const signup = catchAsync(async (req, res, next) => {
   res.status(200).render('signup', {
     title: 'Create your account',
+    user: req.user,
   });
 });
 
-const getAccount = (req, res) => {
+const getAccout = catchAsync(async (req, res, next) => {
   res.status(200).render('account', {
     title: 'Your account',
+    user: req.user,
   });
-};
+});
+const updateUserData = catchAsync(async (req, res, next) => {
+  const updatedUser = await User.findByIdAndUpdate(
+    req.user.id,
+    {
+      name: req.body.name,
+      email: req.body.email,
+    },
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
 
-export { getTour, getView, login, signup, getAccount };
+  res.status(200).render('account', {
+    title: 'Your account',
+    user: updatedUser,
+  });
+});
+
+export { getTour, getView, login, signup, getAccout, updateUserData };
