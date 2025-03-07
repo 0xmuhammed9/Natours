28342ac2ac77f48f3fa2e1623b94c22ac242aae4
0xmuhammed9 <muhammed9alii9@gmail.com>
2025-03-07 10:58:45 +0200

    Create view-route.js

---
+++ routes/view-route.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import { isLoggedIn } from '../controllers/auth-controller.js';
+import {
+  getView,
+  getTour,
+  login,
+  signup,
+  getAccout,
+  updateUserData,
+} from '../controllers/view-controller.js';
+
+const viewRouter = express.Router();
+
+viewRouter.use(isLoggedIn);
+
+/**
+ * ****************************************************************************************
+ *                                  Routes
+ * ****************************************************************************************
+ */
+
+viewRouter.get('/', getView);
+viewRouter.get('/tour/:slug', getTour);
+viewRouter.get('/login', login);
+viewRouter.get('/signup', signup);
+viewRouter.get('/me', getAccout);
+viewRouter.post('update-data', updateUserData);
+
+export default viewRouter;
