c46e33338c927de6b116744d94a526a629a8794d
0xmuhammed9 <muhammed9alii9@gmail.com>
2025-03-07 20:01:19 +0200

    Update index.js

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,5 @@
 /* eslint-disable */
 import { login, logout } from './login.js';
-import { showAlert } from './alerts.js';
 import { updateSettings } from './updateSetting.js';
 
 const loginForm = document.querySelector('.form--login');
@@ -25,30 +24,31 @@ if (userDataForm)
     updateSettings({ name, email }, 'data');
   });
 
-  if (userDataForm)
-    userDataForm.addEventListener('submit', e => {
-      e.preventDefault();
-      const name = document.getElementById('name').value;
-      const email = document.getElementById('email').value;
-      updateSettings({ name, email }, 'data');
-    });
-  
-  if (userPasswordForm)
-    userPasswordForm.addEventListener('submit', async e => {
-      e.preventDefault();
-      document.querySelector('.btn--save-password').textContent = 'Updating...';
-  
-      const password = document.getElementById('password-current').value;
-      const newPassword = document.getElementById('password').value;
-      const passwordConfirm = document.getElementById('password-confirm').value;
-      await updateSettings(
-        { password, newPassword, passwordConfirm },
-        'password'
-      );
-  
-      document.querySelector('.btn--save-password').textContent = 'Save password';
-      document.getElementById('password-current').value = '';
-      document.getElementById('password').value = '';
-      document.getElementById('password-confirm').value = '';
-    });
-  
+if (userDataForm)
+  userDataForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const form = new FormData();
+    form.append('name', document.getElementById('name').value);
+    form.append('email', document.getElementById('email').value);
+    form.append('photo', document.getElementById('photo').files[0]);
+    updateSettings(form, 'data');
+  });
+
+if (userPasswordForm)
+  userPasswordForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    document.querySelector('.btn--save-password').textContent = 'Updating...';
+
+    const password = document.getElementById('password-current').value;
+    const newPassword = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('password-confirm').value;
+    await updateSettings(
+      { password, newPassword, passwordConfirm },
+      'password'
+    );
+
+    document.querySelector('.btn--save-password').textContent = 'Save password';
+    document.getElementById('password-current').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('password-confirm').value = '';
+  });
