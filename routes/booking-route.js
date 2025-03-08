5b63262c8599cca7be27748ec10c176e8f6b0149
0xmuhammed9 <muhammed9alii9@gmail.com>
2025-03-08 17:46:04 +0200

    Create booking-route.js

---
+++ routes/booking-route.js
@@ -0,0 +1,11 @@
+import expres from 'express';
+import { protect } from '../controllers/auth-controller.js';
+import getCheckoutSession from '../controllers/booking-controller.js';
+
+const bookingRoute = expres.Router();
+
+bookingRoute
+  .route('/checkout-session/:tourId')
+  .get(protect, getCheckoutSession);
+
+export default bookingRoute;
